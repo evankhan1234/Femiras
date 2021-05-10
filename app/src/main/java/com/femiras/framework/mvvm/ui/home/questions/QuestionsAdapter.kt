ca5cf7497794710520f845004fa9190b353d071d
MD. Sarawer Jahan Evan <s.jahan@nexdecade.com>
2021-05-10 15:43:23 +0600

    Update Package

--- app/src/main/java/com/femiras/framework/mvvm/ui/home/questions/QuestionsAdapter.kt
+++ app/src/main/java/com/femiras/framework/mvvm/ui/home/questions/QuestionsAdapter.kt
@@ -1,29 +1,25 @@
 package com.femiras.framework.mvvm.ui.home.questions
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.util.Log
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.femiras.framework.mvvm.R
-import com.femiras.framework.mvvm.databinding.LayoutStressBinding
 import com.femiras.framework.mvvm.ui.home.today.sex.SexListener
 import com.tooltip.Tooltip
+import kotlinx.android.synthetic.main.fragment_splash_screen.view.*
 import kotlinx.android.synthetic.main.layout_question_poll.view.*
+import kotlinx.android.synthetic.main.layout_resources_type.view.*
 
 
 class QuestionsAdapter (val context: Context, val sexListener: SexListener) : RecyclerView.Adapter<QuestionsAdapter.CustomViewHolder>() {
 
     private  var mTooltip: Tooltip? =null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        val inflater = LayoutInflater.from(parent?.context)
+        val inflater = LayoutInflater.from(parent.context)
         val cellForRow = inflater.inflate(R.layout.layout_question_poll, parent, false)
         return CustomViewHolder(cellForRow)
     }
@@ -36,21 +32,41 @@ class QuestionsAdapter (val context: Context, val sexListener: SexListener) : Re
 
     override fun onBindViewHolder(holder:CustomViewHolder, position: Int) {
 
-        if (position==0){
+        if (position==0 || position==2 || position==4){
 
             val rg = RadioGroup(context)
-            for (i in 0..4){
+
+
+            for (i in 0..3){
 
                 val radioButton = RadioButton(context)
-                radioButton.text = "Gym and Exerciise"
+                if(i==0){
+                    radioButton.text = "Gym and Exerciise"
+                }
+                else if(i==1){
+                    radioButton.text = "Follow Good Habit"
+                }
+                else if(i==2){
+                    radioButton.text = "Eat Healthy Food and Nutrition"
+                }
+                else if(i==3){
+                    radioButton.text = "Run Faster Like As Boult"
+                }
+                radioButton.background = context.resources.getDrawable(R.drawable.background_radio)
+                radioButton.setButtonDrawable(context.getResources().getDrawable(android.R.color.transparent));
                 radioButton.id=i
                 val params = RadioGroup.LayoutParams(
-                    RadioGroup.LayoutParams.WRAP_CONTENT,
-                    RadioGroup.LayoutParams.WRAP_CONTENT
+                    RadioGroup.LayoutParams.MATCH_PARENT,
+                    125
                 )
+                params.setMargins(25, 15, 15, 25)
+                radioButton.setLayoutParams(params)
+                radioButton.setPadding(40,0,0,0)
+                radioButton.setTextColor(context.getResources().getColorStateList(R.color.selected));
                 radioButton.setOnClickListener{
                     Log.e("dd","df"+radioButton.id )
                 }
+
                 rg.check(radioButton.id)
                 rg.addView(radioButton, params)
 
@@ -59,24 +75,54 @@ class QuestionsAdapter (val context: Context, val sexListener: SexListener) : Re
             holder.itemView.ll.addView(rg)
         }
         else{
-            for (i in 1..5){
+            for (i in 0..5){
                 val cb = CheckBox(context)
-                cb.text = "Listen to your body"
-                holder.itemView.ll.addView(cb)
+              //   cb.background = context.resources.getDrawable(R.drawable.rounded_button)
+                 cb.setButtonDrawable(context.getResources().getDrawable(R.drawable.checkbox_selector))
+                if(i==0){
+                    cb.text = "Gym and Exerciise"
+                }
+                else if(i==1){
+                    cb.text = "Follow Good Habit"
+                }
+                else if(i==2){
+                    cb.text = "Eat Healthy Food and Nutrition"
+                }
+                else if(i==3){
+                    cb.text = "Run Faster Like As Boult"
+                }
+                else if(i==4){
+                    cb.text = "Listen to your body"
+                }
+                else if(i==5){
+                    cb.text = "Music,Movies,Cooking and Drama"
+                }
+                cb.elevation=10f
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    60
+                )
+                cb.setPadding(20,0,0,0)
+                params.setMargins(25, 15, 15, 25)
+                holder.itemView.ll.addView(cb,params)
+
             }
         }
 
 
         holder.itemView.btn.setOnClickListener{ v->
 
-          sexListener.onLoad("1")
+            if (position==0 || position==3 || position==5){
+                sexListener.onLoad("2")
+            }
+            else{
+                sexListener.onLoad("1")
+            }
+
 
 
         }
     }
 
-    inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
-
-    }
+    inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
--- app/src/main/java/com/femiras/framework/mvvm/ui/home/questions/QuestionsFragment.kt
+++ app/src/main/java/com/femiras/framework/mvvm/ui/home/questions/QuestionsFragment.kt
@@ -3,19 +3,18 @@ package com.femiras.framework.mvvm.ui.home.questions
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.femiras.framework.mvvm.R
 import com.femiras.framework.mvvm.databinding.LayoutResultBinding
-import com.femiras.framework.mvvm.databinding.LayoutStressBinding
-import com.femiras.framework.mvvm.ui.home.resource.ResourceAdapter
-import com.femiras.framework.mvvm.ui.home.resource.ResourceTypeAdapter
 import com.femiras.framework.mvvm.ui.home.today.sex.SexListener
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_questions.*
+
 
 @AndroidEntryPoint
 class QuestionsFragment : Fragment() , SexListener {
@@ -43,17 +42,29 @@ class QuestionsFragment : Fragment() , SexListener {
 
 
     }
-    fun showStressDialog() {
+    fun showStressDialog( type:String) {
         val playlistBinding = LayoutResultBinding.inflate(this.layoutInflater)
         val dialogBuilder = android.app.AlertDialog.Builder(requireContext()).setView(playlistBinding.root)
         val alertDialog: android.app.AlertDialog = dialogBuilder.create().apply {
             window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             show()
         }
+        if(type.equals("2")){
+            playlistBinding.dialogTitleTextView1.text="Correct Answer : The Executor framework is a framework for standardizing invocation, scheduling, execution, and control of asynchronous tasks according to a set of execution policies."
+            val img = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_cancel_24);
+            playlistBinding.dialogTitleTextView.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null)
+            //playlistBinding.dialogTitleTextView.setCompoundDrawables(requireContext().getResources().getDrawable(R.drawable.ic_baseline_send_24),null,null,null)
+            playlistBinding.dialogTitleTextView.text="Wrong Answer"
+            playlistBinding.dialogTitleTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.secondaryTextColor))
+            playlistBinding.dialogTitleTextView1.setTextColor(ContextCompat.getColor(requireContext(),R.color.purple))
+        }
+
+
+        playlistBinding.radioHadSex.setOnClickListener { alertDialog.dismiss() }
 
     }
     override fun onLoad(type: String) {
-        showStressDialog()
+        showStressDialog(type)
     }
 
 }
